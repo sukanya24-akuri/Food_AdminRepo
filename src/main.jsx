7c8ddb48c7919d5d0b748e00269ab00ca4d65a4e
Sukanya Akuri <sukanyaakuri@gmail.com>
2025-05-22 03:16:54 +0530

    delployment error changes- done

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import'bootstrap-icons/font/bootstrap-icons.css';
 import { BrowserRouter } from 'react-router-dom';
+
+
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <App/>
